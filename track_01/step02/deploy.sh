93b410fe9be67612005913cc48a10d5c83da7685
iamtaewan <iamtaewan@gmail.com>
2023-11-23 05:42:15 +0900

    first commit

---
+++ track_01/step02/deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+fn -v deploy --app dpws1_pipeline_app
---
+++ track_01/step04/config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+fn config function dpws1_pipeline_app converting_file_fn bucket_name <<#Please Update #RES#04 value in Excel File>>
+
+
